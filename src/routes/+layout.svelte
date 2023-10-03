a7019d154fae7b466be88e979a16df3f6fdc3688
Kasey Soriano <104530889+kaseys117@users.noreply.github.com>
2023-10-02 22:22:07 -0500

    added reset.css and normalize.css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <!-- UI components that should be visible in all screens go here (ex: header, menu, etc.) -->
-<script>
-    /** @type {import('./$types').LayoutData} */
-    export let data;
-</script>
+<style>
+    @import '../lib/reset.css';
+    @import '../lib/normalize.css';
+
+    slot {
+        width: 100%;
+        height: 100vh;
+    }
+</style>
+
+
+<slot />
