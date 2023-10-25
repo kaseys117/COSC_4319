7551c3c46129e5dce45b4b2e60faeaa89e80bc1a
Kasey Soriano <104530889+kaseys117@users.noreply.github.com>
2023-10-24 19:16:11 -0500

    Added styling for landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- Landing page for WellMo -->
+<script>
+    // No script needed for this simple page
+</script>
+
+<div class="container">
+    <div class="header">WellMo</div>
+    <a href="/login">
+        <button class="button">Log In</button>
+    </a>
+    <a href="/register">
+        <button class="button">Register</button>
+    </a>
+</div>
+
+<style>
+    /* TODO: Change to css grid */
+    .container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        place-items: center;
+        height: 100vh;
+        gap: 1em;
+    }
+
+    .header {
+        font-size: 2em;
+        font-weight: bold;
+        margin-bottom: 1em;
+    }
+
+    .button {
+        padding: 10px 20px;
+        font-size: 1em;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        margin: auto;
+    }
+</style>
