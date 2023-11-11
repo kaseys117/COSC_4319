137f46ba54e58246a6203fc4e841d73caf671c23
Kasey Soriano <104530889+kaseys117@users.noreply.github.com>
2023-11-11 15:10:18 -0600

    Added simple log out button in /app/settings

---
+++ src/routes/app/settings/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { goto } from "$app/navigation";
+    export let data;
+    let { supabase } = data;
+    $: ({ supabase } = data);
+
+    const handleSignOut = async () => {
+        const { error } = await supabase.auth.signOut();
+        if (error) {
+            console.log(error);
+        } else {
+            goto("/");
+        }
+    };
+</script>
+
+<button class="error" on:click={handleSignOut}>Sign out</button>
