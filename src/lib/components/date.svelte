da356c3ada08c69750bf472cc1d6298dbee01222
Kasey Soriano <104530889+kaseys117@users.noreply.github.com>
2023-11-12 14:30:43 -0600

    renamed today_date component to date

---
+++ src/lib/components/date.svelte
@@ -0,0 +1,17 @@
+<script>
+    const today = new Date().toLocaleDateString("en-US", {
+        weekday: "long",
+        month: "long",
+        day: "numeric",
+    });
+</script>
+
+<strong>
+    {today}
+</strong>
+
+<style>
+    strong {
+        font-size: 1.25em;
+    }
+</style>
