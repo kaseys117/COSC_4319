6ff6e3541d39da1422d27d4e7cd1e7a7bd76f0d0
Kasey Soriano <104530889+kaseys117@users.noreply.github.com>
2023-11-12 22:45:51 -0600

    Logging Mood is stable, added process_row function

--- src/lib/components/history_list.svelte
+++ src/lib/components/history_list.svelte
@@ -1,20 +1,35 @@
 <script>
-    import { duration } from "$lib/utils.js";
+    import { duration, formatDate } from "$lib/utils.js";
     export let col_name = "Value";
     export let data = [];
+
+    function process_row(row) {
+        if (col_name == "Hours") {
+            // Sleep Data
+            return duration(row.start, row.end);
+        } else if (col_name == "Mood") {
+            // Mood Data
+            return row.mood;
+        }
+    }
 </script>
 
 <div class="table-scroll">
     <table>
         <tr>
             <th class="date">Date</th>
-            <th class="hours">{col_name}</th>
+            <th class="value">{col_name}</th>
         </tr>
         <tbody>
+            {#if data.length === 0}
+                <tr>
+                    <td colspan="2" style="text-align: center">No data</td>
+                </tr>
+            {/if}
             {#each data as row}
                 <tr>
-                    <td class="date">{row.date}</td>
-                    <td class="hours">{duration(row.start, row.end)}</td>
+                    <td class="date">{formatDate(row.date)}</td>
+                    <td class="value">{process_row(row)}</td>
                 </tr>
             {/each}
         </tbody>
@@ -36,9 +51,6 @@
         padding: 0.75em;
     }
 
-    .hours {
-        text-align: right;
-    }
     tbody > tr {
         background-color: var(--color-surface-100);
     }
@@ -48,10 +60,10 @@
         background-color: var(--color-surface-50);
         padding: 0.5em;
     }
-    th.date {
+    .date {
         text-align: left;
     }
-    th.hours {
+    .value {
         text-align: right;
     }
 </style>
--- src/routes/app/log/mood/+page.svelte
+++ src/routes/app/log/mood/+page.svelte
@@ -1,1 +1,85 @@
-<p>log mood</p>
+<script>
+    import Happy from "~icons/mdi/emoticon-excited-outline";
+    import Neutral from "~icons/mdi/emoticon-neutral-outline";
+    import Sad from "~icons/mdi/emoticon-cry-outline";
+    import Angry from "~icons/mdi/emoticon-angry-outline";
+    import Tired from "~icons/mdi/sleep";
+    import Sick from "~icons/mdi/emoticon-sick-outline";
+    import Stressed from "~icons/mdi/emoticon-dead-outline";
+    import Confused from "~icons/mdi/emoticon-confused-outline";
+    import Today from "$lib/components/date.svelte";
+    import { enhance } from "$app/forms";
+
+    export let data;
+    const uid = data.session.user.id;
+</script>
+
+<h1>How are you feeling?</h1>
+
+<Today />
+
+<form method="POST" use:enhance>
+    <input type="hidden" name="uid" value={uid} required />
+    <label>
+        <i><Happy /></i>
+        Happy
+        <input type="radio" name="mood" value="Happy" required />
+    </label>
+    <label>
+        <i><Neutral /></i>
+        Neutral
+        <input type="radio" name="mood" value="Neutral" required />
+    </label>
+    <label>
+        <i><Sad /></i>
+        Sad
+        <input type="radio" name="mood" value="Sad" required />
+    </label>
+    <label>
+        <i><Angry /></i>
+        Angry
+        <input type="radio" name="mood" value="Angry" required />
+    </label>
+    <label>
+        <i><Tired /></i>
+        Tired
+        <input type="radio" name="mood" value="Tired" required />
+    </label>
+    <label>
+        <i><Sick /></i>
+        Sick
+        <input type="radio" name="mood" value="Sick" required />
+    </label>
+    <label>
+        <i><Stressed /></i>
+        Stressed
+        <input type="radio" name="mood" value="Stressed" required />
+    </label>
+    <label>
+        <i><Confused /></i>
+        Confused
+        <input type="radio" name="mood" value="Confused" required />
+    </label>
+    <textarea class="border" name="notes" rows="5" placeholder="Notes" />
+    <a href="/app/mood" class="btn-secondary">Cancel</a>
+    <button type="submit" class="btn-secondary">Submit</button>
+</form>
+
+<style>
+    /* TODO Fix styling (refer to the Figma design) */
+    /* used to change the size of the icons/emoticons */
+    i {
+        font-size: 3.5em;
+    }
+    form {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+    }
+    textarea {
+        padding: 1em;
+        grid-column: 1 / -1;
+    }
+    a {
+        text-decoration: none;
+    }
+</style>
