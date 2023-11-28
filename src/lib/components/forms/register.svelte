13eabcedbe4c977d26b78d4eceadfb35343b90c7
Kasey Soriano <104530889+kaseys117@users.noreply.github.com>
2023-11-27 22:57:00 -0600

    Updated registration page

--- src/lib/components/forms/register.svelte
+++ src/lib/components/forms/register.svelte
@@ -1,6 +1,5 @@
 <script>
     import { enhance } from "$app/forms";
-    export let form;
     let password = "";
     let hasCapital;
     let hasLower;
@@ -8,6 +7,7 @@
     let hasNum;
     let minLength;
     let notValidPassword = true;
+    let showRequirements = false;
 
     function validatePassword() {
         // Checks if the password has at least 1 capital letter, 1 lowercase letter, 1 special character, 1 number, and is at least 8 characters long
@@ -36,46 +36,96 @@
 <form method="POST" use:enhance>
     <!-- TODO: Use zod and SuperForms for validation -->
     <input
-        class="input-field"
-        type="text"
-        placeholder="nickname"
-        name="nickname"
-        required
-    />
-    <input
-        class="input-field"
         name="email"
+        class="input-field"
         type="email"
         placeholder="email"
-        value={form?.email ?? ""}
-        pattern="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$"
+        autocomplete="email"
+        pattern="[a-zA-Z0-9+_.\-]+@[a-zA-Z0-9.\-]+"
         title="must be a valid email address"
         required
     />
     <input
-        class="input-field"
         name="password"
+        class="input-field"
         type="password"
         placeholder="password"
+        autocomplete="new-password"
         bind:value={password}
         on:input={validatePassword}
+        on:focusin={() => {
+            showRequirements = true;
+        }}
+        on:focusout={() => {
+            showRequirements = false;
+        }}
         required
     />
-
-    <button
-        class="btn-primary"
-        on:click={validatePassword}
-        disabled={notValidPassword}>Register</button
-    >
+    {#if showRequirements}
+        <ul>
+            <!-- Change the class (which changes the ❌ to a ✅) if the password satisfies the requirement -->
+            <li class:ok={minLength}>At least 8 characters</li>
+            <li class:ok={hasCapital}>At least 1 capital letter</li>
+            <li class:ok={hasLower}>At least 1 lowercase letter</li>
+            <li class:ok={hasNum}>At least 1 number</li>
+            <li class:ok={hasSpecial}>At least 1 special character</li>
+        </ul>
+    {/if}
+    <input
+        name="nickname"
+        class="input-field"
+        type="text"
+        placeholder="first name"
+        required
+    />
+    <select name="sex" class="input-field">
+        <option value="">sex</option>
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+    </select>
+    <label for="dob" class="dob-label">Date of birth</label>
+    <input
+        name="dob"
+        class="input-field dob"
+        type="date"
+        id="dob"
+        min="1923-01-01"
+        max="2023-12-31"
+    />
+    <input
+        name="height-ft"
+        type="number"
+        class="input-field height-ft"
+        min="3"
+        max="8"
+        placeholder="height (ft)"
+        required
+    />
+    <input
+        name="height-in"
+        type="number"
+        class="input-field height-in"
+        min="0"
+        max="11"
+        placeholder="height (in)"
+        required
+    />
+    <!-- Using enumerated type "activity level" on the database -->
+    <select name="activity-level" class="input-field activity">
+        <option value="">Activity Level</option>
+        <option value="sedentary">Sedentary</option>
+        <option value="light">Light (1-2 days/week)</option>
+        <option value="moderate">Moderate (3-5 days/week)</option>
+        <option value="high">High (6-7 days/week)</option>
+        <option value="intense"
+            >Intense (strenuous exercise/sports & physical job or training twice
+            a day)</option
+        >
+    </select>
+    <button type="submit" class="btn-primary" class:disabled={notValidPassword}>
+        Register
+    </button>
 </form>
-<ul>
-    <!-- Change the class (which changes the ❌ to a ✅) if the password satisfies the requirement -->
-    <li class:ok={minLength}>At least 8 characters</li>
-    <li class:ok={hasCapital}>At least 1 capital letter</li>
-    <li class:ok={hasLower}>At least 1 lowercase letter</li>
-    <li class:ok={hasNum}>At least 1 number</li>
-    <li class:ok={hasSpecial}>At least 1 special character</li>
-</ul>
 
 <style>
     h1 {
@@ -102,5 +152,24 @@
         display: grid;
         grid-template-rows: auto auto 1fr;
         gap: 0.5em;
+    }
+    .dob {
+        grid-column: 1;
+        grid-row: 5;
+        justify-self: end;
+    }
+    .dob-label {
+        grid-row: 5;
+        grid-column: 1;
+        align-self: center;
+        width: max-content;
+    }
+    .disabled {
+        background-color: #cccccc;
+        color: #666666;
+        font-weight: 500;
+    }
+    .activity {
+        width: 100%;
     }
 </style>
--- src/routes/auth/register/+page.server.js
+++ src/routes/auth/register/+page.server.js
@@ -1,5 +1,6 @@
 import { fail, redirect } from '@sveltejs/kit'
 import { AuthApiError } from '@supabase/supabase-js'
+import { activity_level_enum } from '$lib/utils.js'
 
 export const actions = {
   default: async ({ request, locals: { supabase } }) => {
@@ -7,6 +8,22 @@ export const actions = {
     const email = formData.get('email') // get email from form data
     const password = formData.get('password') // get password from form data
     const nickname = formData.get('nickname') // get nickname from form data
+    const sex = formData.get('sex')
+    const dob = formData.get('dob')  // get date of birth from form data
+    const height_ft = formData.get('height-ft') // get height from form data
+    const height_in = formData.get('height-in') // get height from form data
+    const height = `${height_ft}'${height_in}"` // combine height_ft and height_in into one string
+    const activity_level = formData.get('activity-level') // get activity level from form data
+
+    // Check that activity level is valid
+    if (!Object.values(activity_level_enum).includes(activity_level)) {
+      return fail(400, {
+        error: 'Invalid activity level.',
+        values: {
+          activity_level
+        }
+      })
+    }
 
     // register user with email and password, and store the nickname in the metadata
     const { error } = await supabase.auth.signUp({
@@ -14,7 +31,11 @@ export const actions = {
       password,
       options: {
         data: {
-          nickname: nickname
+          nickname: nickname,
+          sex: sex,
+          dob: dob,
+          height: height,
+          activity_level: activity_level
         }
       }
     })
